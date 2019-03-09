74ef6d96e464eaa459edcd3f45babad42b3b5244
Matt Bacher <mattjb1991@sbcglobal.net>
2019-03-09 11:34:13 -0500

    Add files via upload

---
+++ ultimateMVC/ultimateMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ultimateMVC.Models;
+
+namespace ultimateMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<string> _userInputList = new List<string>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var formModel = new HomeModel()
+            {
+                UserInputList = new List<string>()
+            };
+
+            return View(formModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(HomeModel formModel)
+        {
+            var userInput = formModel.UserInput;
+
+            _userInputList.Add(userInput);
+            formModel.UserInputList = _userInputList;
+
+            return View(formModel);
+        }
+
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ultimateMVC/ultimateMVC/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ultimateMVC.Models
+{
+    public class HomeModel
+    {
+        public string UserInput { get; set; }
+        public List<string> UserInputList { get; set; }
+    }
+}
